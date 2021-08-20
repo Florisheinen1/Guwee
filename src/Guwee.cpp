cfbdf8b9be17a9d59f1c83bf177071b50d15cba9
Floris Heinen <Floris.heinen1@gmail.com>
2021-08-21 01:27:56 +0200

    Integrated GLAD with cmake

--- src/Guwee.cpp
+++ src/Guwee.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
 #include "../include/Guwee.hpp"
 
+#include "../deps/glad/include/glad/gl.h"
+
+
+void printErr(const char* errorMessage) {
+	std::cout << "ERROR: " << errorMessage << std::endl;
+}
+void checkGlErrors() {
+	GLenum errorCode;
+	while ((errorCode = glGetError()) != GL_NO_ERROR) {
+		std::string error;
+		switch (errorCode) {
+			case GL_INVALID_ENUM: printErr("INVALID ENUM USED"); break;
+			case GL_INVALID_VALUE: printErr("INVALID VALUE USED"); break;
+			case GL_INVALID_OPERATION: printErr("INVALID OPERATION USED"); break;
+			case GL_INVALID_FRAMEBUFFER_OPERATION: printErr("INVALID FRAMEBUFFER OPERATION USED"); break;
+			case GL_OUT_OF_MEMORY: printErr("OUT OF MEMORY"); break;
+			default: printErr("UNKNOWN ERROR"); break;
+		}
+	}
+}
+
 void Guwee::initGuwee() {
 	std::cout << "Init guwee!" << std::endl;
+	checkGlErrors();
+	std::cout << "Init vao" << std::endl;
+	
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+
+	checkGlErrors();
 }
